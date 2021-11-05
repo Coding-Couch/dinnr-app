6ed9473774a2e01c18d0058594ec0bdbbfe3d121
Brent Mifsud <18543934+BrentMifsud@users.noreply.github.com>
2021-11-05 16:05:01 -0400

    add GitHub project

---
+++ Dinnr/Dinnr/DinnrApp.swift
@@ -0,0 +1,17 @@
+//
+//  DinnrApp.swift
+//  Dinnr
+//
+//  Created by Brent Mifsud on 2021-11-05.
+//
+
+import SwiftUI
+
+@main
+struct DinnrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
